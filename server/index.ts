8208b0f88d61508858324f6cf632c0fac59f8dc1
jrozal <68182930+jrozal@users.noreply.github.com>
2021-07-27 10:27:42 -0700

    Update comments

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,4 @@
-/**
- * Required External Modules
- */
+/* Required External Modules */
 
 import * as dotenv from 'dotenv';
 import express from 'express';
@@ -9,9 +7,7 @@ import helmet from 'helmet';
 
 dotenv.config();
 
-/**
- * App Variables
- */
+/* App Variables */
 
 if (!process.env.PORT) {
   process.exit(1);
@@ -21,17 +17,13 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-/**
- *  App Configuration
- */
+/* App Configuration */
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-/**
- * Server Activation
- */
+/* Server Activation */
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
