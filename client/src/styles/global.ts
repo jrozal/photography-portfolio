603155bb08bbf6e501643b1354cfed3123592240
jrozal <68182930+jrozal@users.noreply.github.com>
2021-07-31 11:55:01 -0700

    Add global style

---
+++ client/src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle `
+  @import url('https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap');
+
+  *{
+    margin: 0;
+    padding: 0;
+    outline:0;
+    box-sizing:border-box;
+    font-family: 'PT Sans', sans-serif;
+  }
+
+  #root{
+    margin:0 auto;
+  }
+
+  h1 {
+    font-weight: 700;
+  }
+`;
