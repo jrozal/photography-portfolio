6ba5007c9c3ec146e8d8bec8dc3f983d63151829
jrozal <68182930+jrozal@users.noreply.github.com>
2021-07-31 00:32:03 -0700

    Add title component

---
+++ client/src/components/Title.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-size: 28px;
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+const Title = () => (
+  <Heading>Justin Rozal Photography</Heading>
+);
+
+export default Title;
