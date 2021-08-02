d465709c9e561b8a6452e512cfb5064f34c3a271
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-02 00:28:26 -0700

    [WIP] Footer component

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as Styled from './styled';
+
+const Footer: React.FC = () => (
+  <Styled.FooterContiner>
+    <div>All photos subject to Copyright © 2021 Justin Rozal. All rights reserved.</div>
+    <div>Coded by Justin</div>
+  </Styled.FooterContiner>
+);
+
+export default Footer;
