9058328d07b333f01c2ce4b9943abade3c7ad657
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-01 20:32:22 -0700

    Add socials data

---
+++ client/src/data/socials.ts
@@ -0,0 +1,29 @@
+export const socialsData = [
+  {
+    name: 'Instagram',
+    values: {
+      ariaLabel: 'Open Instagram profile for Justin Rozal',
+      href: 'https://www.instagram.com/justin.r/',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    }
+  },
+  {
+    name: 'GitHub',
+    values: {
+      ariaLabel: 'Open GitHub profile for Justin Rozal',
+      href: 'https://github.com/jrozal',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    }
+  },
+  {
+    name: 'LinkedIn',
+    values: {
+      ariaLabel: 'Open LinkedIn profile for Justin Rozal',
+      href: 'https://www.linkedin.com/in/jrozal/',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    }
+  },
+];
