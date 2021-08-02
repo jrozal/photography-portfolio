68175a568638bb3ec0ee7f93e790937f25cd5c81
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-01 20:54:12 -0700

    Add styled Title component

---
+++ client/src/components/Title/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './styled';
+
+const Title: React.FC = () => (
+  <Styled.HeadingContainer>
+    <Styled.Heading>
+      Justin Rozal Photography
+    </Styled.Heading>
+  </Styled.HeadingContainer>
+);
+
+export default Title;
---
+++ client/src/components/Title/styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HeadingContainer = styled.div`
+  padding: 50px 0 40px 0;
+`;
+
+export const Heading = styled.h1`
+  font-size: 28px;
+  text-transform: uppercase;
+  text-align: center;
+`;
