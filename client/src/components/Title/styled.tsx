648417f127217d9ce0bd4e467e14fc896b874739
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-01 22:55:19 -0700

    Convert px to rem

--- client/src/components/Title/styled.tsx
+++ client/src/components/Title/styled.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const HeadingContainer = styled.header`
-  padding: 50px 0 40px 0;
+  padding: 3.125rem 0 2.5rem 0;
 `;
 
 export const Heading = styled.h1`
-  font-size: 28px;
+  font-size: 1.75rem;
   text-transform: uppercase;
   text-align: center;
 `;
