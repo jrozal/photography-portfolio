5e2ce0e18b5f7a53728fbdf98895fe1cc87a8ecd
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-02 22:02:22 -0700

    Add mock photo data

---
+++ client/src/data/mock-photo-data.ts
@@ -0,0 +1,12 @@
+export const mockPhotoData = [
+  'https://format-com-cld-res.cloudinary.com/image/private/s--AmEFGYlN--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/0c296c9598c63387bf6dcbe48c2ac068/IMG_9240-1-2_copy.jpg?1600',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--E1EMnyGV--/c_limit,g_center,h_65535,w_900/fl_keep_iptc.progressive,q_95/v1/28e1df8ca4dc33305797bcb9d182c991/IMG_9933-1-1-2.jpg?900',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--zSmDB4M3--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/e56c5bb4696013d6012d0376e90d5029/IMG_9773-1-3_copy.jpg?1600',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--NZB_f1ZB--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/ea3b8b5507f43ee76f108b124273cafd/IMG_1052-2-1.jpg?1600',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--PVISHDej--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/b802e4dc85ff86573da3093980cafba2/IMG_9892-1-1.jpg?1600',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--S57R74JI--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/747fe42a82f8bdc2d8be92eaf8ddda9f/IMG_4969-1.jpg?1600',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--N1G_21q1--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/304cf88ecea8515bcf12efd7ec4e5321/IMG_9959-full-4000-2.jpg?1600',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--ARBZI6Ky--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/be6206de5d83787e64cc47722870e9c8/IMG_4292-5.jpg?1600',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--6p_MxyeZ--/c_limit,g_center,h_65535,w_900/fl_keep_iptc.progressive,q_95/v1/d8e665e1dc391a6848af7faf30a5c629/IMG_2493-1-2.jpg?900',
+  'https://format-com-cld-res.cloudinary.com/image/private/s--ZtNFm-A6--/c_limit,g_center,h_65535,w_1600/fl_keep_iptc.progressive,q_95/v1/4c58f0e2a314892ef9b26d07cd9a0024/IMG_0016-1-1.jpg?1600'
+];
